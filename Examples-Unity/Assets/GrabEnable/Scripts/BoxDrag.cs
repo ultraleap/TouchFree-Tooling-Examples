ab534ad1381dc7d0d7bff3b9a8453bd39d75a5f1
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-10-21 13:51:22 +0100

    Add an example for enabling a grab interaction

---
+++ Examples-Unity/Assets/GrabEnable/Scripts/BoxDrag.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Ultraleap.TouchFree.Tooling;
+
+public class BoxDrag : MonoBehaviour, 
+                        IPointerDownHandler, 
+                        IPointerUpHandler,
+                        IPointerExitHandler
+{
+    private RectTransform boxTransform;
+
+    private bool grabbed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxTransform = GetComponent<RectTransform>();
+        InputActionManager.TransmitInputAction += UpdateBox;
+    }
+
+    // Update is called once per frame
+    public void UpdateBox(InputAction _action)
+    {
+        if (grabbed && _action.InputType == InputType.MOVE)
+        {
+            float lerpSpeed = 0.5f;
+            Vector2 position = _action.CursorPosition;
+            boxTransform.anchoredPosition = Vector2.Lerp(boxTransform.anchoredPosition, position, lerpSpeed);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData _data)
+    {
+        grabbed = true;
+    }
+
+    public void OnPointerUp(PointerEventData _data)
+    {
+        grabbed = false;
+    }
+    public void OnPointerExit(PointerEventData _data)
+    {
+        grabbed = false;
+    }
+
+}
---
+++ Examples-Unity/Assets/GrabEnable/Scripts/EnableGrabInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ultraleap.TouchFree.Tooling.Configuration;
+
+public class EnableGrabInteraction : MonoBehaviour
+{
+    InteractionConfig interactionConfigRequest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Interaction Config
+        interactionConfigRequest = new InteractionConfig();
+        interactionConfigRequest.interactionType = Ultraleap.TouchFree.Tooling.InteractionType.GRAB;
+        interactionConfigRequest.useScrollingOrDragging = true;
+
+        ChangeInteractionConfig();
+    }
+
+    public void ChangeInteractionConfig()
+    {
+        ConfigurationManager.RequestConfigChange(interactionConfigRequest, null, ChangeResponseCallback);
+    }
+
+    void ChangeResponseCallback(Ultraleap.TouchFree.Tooling.Connection.WebSocketResponse _response)
+    {
+        Debug.Log(JsonUtility.ToJson(_response));
+    }
+}
