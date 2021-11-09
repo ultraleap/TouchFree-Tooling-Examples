a5e8b09ba5f28b360bdd6b79e058b7f7274e1d74
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-11-09 14:48:30 +0000

    Example showing how to use TF events to interaction with non-ui elements

---
+++ Examples-Unity/Assets/Non-UI-Input/TFSelectObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ultraleap.TouchFree.Tooling;
+using Ultraleap.TouchFree.Tooling.Connection;
+
+public class TFSelectObject : MonoBehaviour
+{
+    public Transform cursorObject;
+    public Transform sceneObjects;
+    
+    public InputAction currentAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputActionManager.TransmitInputAction += HandleInputAction;
+        ConnectionManager.HandsLost += Reset;
+    }
+
+    void Update()
+    {
+        if (currentAction.InputType == InputType.DOWN)
+        {
+            print("Down");
+        }
+    }
+
+    public void HandleInputAction(InputAction _action)
+    {
+        currentAction = _action;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(_action.CursorPosition), out hit))
+        {
+            cursorObject.position = hit.point;
+            
+            if (_action.InputType == InputType.DOWN)
+            {
+                hit.transform.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        for(int i = 0; i < sceneObjects.childCount; i++)
+        {
+            sceneObjects.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
