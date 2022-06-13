f2a4167379a629559c1599e0b88068b8b3cc385b
James Provan <james.provan@ultrahaptics.com>
2022-06-13 10:13:09 +0100

    Modify cursor so that the tip is the origin point rather than the center of the img.

--- Examples-Web/SystemCursor/SystemCursorExample.js
+++ Examples-Web/SystemCursor/SystemCursorExample.js
@@ -18,24 +18,30 @@ function AddTouchFreeCursor()
     cursor.style.position = "absolute";
     cursor.width = 150;
     cursor.height = 150;
-    cursor.style.zIndex = "1001";
+    cursor.style.zIndex = "9001";
     cursor.style.transform = "rotate(-30deg)";
+    cursor.style.transformOrigin = "50% 0%";
+    cursor.style.transform = "translate(-50%, 0%) rotate(-30deg)";
 
     var cursorBorder = document.createElement('img');
     cursorBorder.src = "./images/CursorBorderBlackNoTail.png";
     cursorBorder.style.position = "absolute";
     cursorBorder.width = 150;
     cursorBorder.height = 150;
-    cursorBorder.style.zIndex = "1003";
+    cursorBorder.style.zIndex = "9003";
     cursorBorder.style.transform = "rotate(-30deg)";
+    cursorBorder.style.transformOrigin = "50% 0%";
+    cursorBorder.style.transform = "translate(-50%, 0%) rotate(-30deg)";
 
     var cursorFill = document.createElement('img');
     cursorFill.src = "./images/CursorFillBlackNoTail.png";
     cursorFill.style.position = "absolute";
     cursorFill.width = 150;
     cursorFill.height = 150;
-    cursorFill.style.zIndex = "1002";
+    cursorFill.style.zIndex = "9002";
     cursorFill.style.transform = "rotate(-30deg)";
+    cursorFill.style.transformOrigin = "50% 0%";
+    cursorFill.style.transform = "translate(-50%, 0%) rotate(-30deg)";
 
     // This is a special class used by the WebInputController to identify the html elements that
     // make up the cursor. This is so it can ignore cursor-related objects when it is looking
@@ -63,13 +69,10 @@ class SystemCursor extends TouchFree.Cursors.TouchlessCursor
     }
 
     UpdateCursor(_inputAction) {
-        this.cursorBorder.style.left = (_inputAction.CursorPosition[0] - (this.cursor.clientWidth / 2)) + "px";
-        this.cursorBorder.style.top = (window.innerHeight - (_inputAction.CursorPosition[1] + (this.cursor.clientHeight / 2))) + "px";
-
-        this.cursorFill.style.left = (_inputAction.CursorPosition[0] - (this.cursor.clientWidth / 2)) + "px";
-        this.cursorFill.style.top = (window.innerHeight - (_inputAction.CursorPosition[1] + (this.cursor.clientHeight / 2))) + "px";
+        let left = _inputAction.CursorPosition[0] + "px";
+        let top = (window.innerHeight - _inputAction.CursorPosition[1]) + "px";
+        this.cursor.style.left = this.cursorBorder.style.left = this.cursorFill.style.left = left;
+        this.cursor.style.top = this.cursorBorder.style.top = this.cursorFill.style.top = top;
         this.cursorFill.style.clipPath = "inset(" + (100 - (_inputAction.ProgressToClick * 100)).toString() + "% 0% 0% 0%)";
-
-        super.UpdateCursor(_inputAction);
     }
 }
