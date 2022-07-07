c6e7f5c460b3ff5a76e4f0d6d9e0da3e13fa5925
Tom Cartwright <tom.cartwright@ultraleap.com>
2022-07-07 09:19:23 +0100

    Removed app test

--- Examples-Web/BorderProgressButtons/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-describe("App", () => {
-  it("should render 4 buttons", () => {
-    const { container } = render(<App />);
-    expect(container.getElementsByClassName("button")).toHaveLength(4);
-  });
-});
