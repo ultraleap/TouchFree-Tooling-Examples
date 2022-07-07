7f09fe181635e305c106299aaddf898d6a9db807
Tom Cartwright <tom.cartwright@ultraleap.com>
2022-07-07 08:55:35 +0100

    Added improved deault button styling

--- Examples-Web/BorderProgressButtons/src/components/button/types.ts
+++ Examples-Web/BorderProgressButtons/src/components/button/types.ts
@@ -1,30 +1,33 @@
 import { CSSProperties } from "react";
 
 export interface ButtonProps {
-  progressToClick: number;
-  clickCallback: (args: any[] | any) => void;
-  simulateTouchFree?: () => void;
-  colour?: string;
+    progressToClick: number;
+    clickCallback: (args: any[] | any) => void;
+    colour?: string;
 }
 
 interface customCSS extends CSSProperties {
-  "--left-width"?: string;
-  "--left-height"?: string;
-  "--right-width"?: string;
-  "--right-height"?: string;
-  "--border-style"?: string;
+    "--left-width"?: string;
+    "--left-height"?: string;
+    "--right-width"?: string;
+    "--right-height"?: string;
+    "--border-style"?: string;
 }
 
+const heightPx = 100;
+const widthPx = 200;
+
 export const defaultButtonStyle: customCSS = {
-  height: "50px",
-  width: "100px",
-  border: 0,
-  borderTopLeftRadius: "10px",
-  borderBottomRightRadius: "10px",
-  transition: "all 0.2s",
-  "--left-width": "97px",
-  "--left-height": "44px",
-  "--right-width": "97px",
-  "--right-height": "44px",
-  "--border-style": "3px solid white",
+    height: `${heightPx}px`,
+    width: `${widthPx}px`,
+    border: 0,
+    borderTopLeftRadius: "10px",
+    borderBottomRightRadius: "10px",
+    transition: "all 0.2s",
+    transform: "scale(1)",
+    "--left-width": `${widthPx - 3}px`,
+    "--left-height": `${heightPx - 3}px`,
+    "--right-width": `${widthPx - 3}px`,
+    "--right-height": `${heightPx - 3}px`,
+    "--border-style": "3px solid white",
 };
