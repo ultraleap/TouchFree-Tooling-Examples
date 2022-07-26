9e7dd3204c885753dd36423b652997f5e2ece120
James Provan <james.provan@ultrahaptics.com>
2022-07-26 11:36:54 +0100

    Adding scrolling input controller example - note this input controller also uses the 'click' event on pointer up.

---
+++ Examples-Web/ScrollInputController/TouchFree_ScrollInputController.js
@@ -0,0 +1,203 @@
+const InteractionType = TouchFree.TouchFreeToolingTypes.InteractionType;
+
+class WebClickScrollInputController extends TouchFree.InputControllers.WebInputController {
+
+    constructor() {
+        super();
+        this.interactionType = -1;
+        this.pointerDownElement = null;
+        this.cancelUpDueToScrolling = false;
+
+        this.scrollTarget;
+        this.scrollDownPos;
+        this.scrollLeftDownPos;
+        this.scrollTopDownPos;
+        this.scrollDownHandler;
+
+        this.primedForUpEvent = false;
+        this.progressToClickUpDispatch = 0.5;
+        this.scrollDistanceThreshold = 25;
+        this.downEventTimestamp;
+
+        TouchFree.Connection.ConnectionManager.AddConnectionListener(() => {
+            TouchFree.Configuration.ConfigurationManager.RequestConfigState(function(state) {
+                this.interactionType = state.interaction.InteractionType;
+            }.bind(this));
+        });
+    }
+
+    HandleInputAction(_inputData) {
+        let invertedCursorPos = [_inputData.CursorPosition[0], _inputData.CursorPosition[1]];
+        let elementAtPos = this.GetTopNonCursorElement(invertedCursorPos);
+        this.activeEventProps.clientX = invertedCursorPos[0];
+        this.activeEventProps.clientY = invertedCursorPos[1];
+
+        if (this.interactionType == InteractionType.PUSH) {
+            this.HandleInputActionPush(_inputData, elementAtPos);
+        } else {
+            this.HandleInputActionDefault(_inputData, elementAtPos);
+        }
+    }
+
+    HandleInputActionPush(_inputData, elementAtPos) {
+        if (this.primedForUpEvent && _inputData.ProgressToClick < this.progressToClickUpDispatch) {
+            this.dispatchUp(elementAtPos);
+        }
+
+        switch (_inputData.InputType) {
+            case TouchFree.TouchFreeToolingTypes.InputType.CANCEL:
+                let cancelEvent = new PointerEvent("pointercancel", this.activeEventProps);
+                if (elementAtPos !== null) {
+                    let parentTree = this.GetOrderedParents(elementAtPos);
+                    parentTree.forEach((parent) => {
+                        if (parent !== null) {
+                            parent.dispatchEvent(cancelEvent);
+                        }
+                    });
+                }
+                if (this.pointerDownElement !== null) {
+                    this.pointerDownElement.dispatchEvent(cancelEvent);
+                    this.pointerDownElement = null;
+                }
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.MOVE:
+                this.HandleMove(elementAtPos);
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.DOWN:
+                let downEvent = new PointerEvent("pointerdown", this.activeEventProps);
+                this.DispatchToTarget(downEvent, elementAtPos);
+                this.downEventTimestamp = _inputData.Timestamp;
+                this.pointerDownElement = elementAtPos;
+                this.cancelUpDueToScrolling = false;
+                this.primedForUpEvent = false;
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.UP:
+                // TouchFree service PUSH interaction will produce an UP event after a 1500ms timeout.
+                // Ensure we pass on the UP event in this case, rather than priming until progressToClickUpDispatch is reached.
+                if (_inputData.Timestamp - this.downEventTimestamp > 1.49e6) {
+                    this.dispatchUp(elementAtPos);
+                    break;
+                }
+                this.primedForUpEvent = true;
+                break;
+        }
+    }
+
+    HandleInputActionDefault(_inputData, elementAtPos) {
+        switch (_inputData.InputType) {
+            case TouchFree.TouchFreeToolingTypes.InputType.CANCEL:
+                let cancelEvent = new PointerEvent("pointercancel", this.activeEventProps);
+                if (elementAtPos !== null) {
+                    let parentTree = this.GetOrderedParents(elementAtPos);
+                    parentTree.forEach((parent) => {
+                        if (parent !== null) {
+                            parent.dispatchEvent(cancelEvent);
+                        }
+                    });
+                }
+                if (this.pointerDownElement !== null) {
+                    this.pointerDownElement.dispatchEvent(cancelEvent);
+                    this.pointerDownElement = null;
+                }
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.MOVE:
+                this.HandleMove(elementAtPos);
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.DOWN:
+                let downEvent = new PointerEvent("pointerdown", this.activeEventProps);
+                this.DispatchToTarget(downEvent, elementAtPos);
+                this.pointerDownElement = elementAtPos;
+                this.cancelUpDueToScrolling = false;
+                break;
+            case TouchFree.TouchFreeToolingTypes.InputType.UP:
+                let upEvent = new PointerEvent("pointerup", this.activeEventProps);
+                this.DispatchToTarget(upEvent, elementAtPos);
+                if (!this.cancelUpDueToScrolling) {
+                    if (elementAtPos !== null) {
+                        let clickEvent = new PointerEvent("click", this.activeEventProps);
+                        this.DispatchToTarget(clickEvent, elementAtPos);
+                    }
+                }
+                this.cancelUpDueToScrolling = false;
+                break;
+        }
+    }
+
+    dispatchUp(elementAtPos) {
+        this.primedForUpEvent = false;
+
+        let upEvent = new PointerEvent("pointerup", this.activeEventProps);
+        this.DispatchToTarget(upEvent, elementAtPos);
+        if (!this.cancelUpDueToScrolling) {
+            if (elementAtPos !== null) {
+                let clickEvent = new PointerEvent("click", this.activeEventProps);
+                this.DispatchToTarget(clickEvent, elementAtPos);
+            }
+        }
+        this.cancelUpDueToScrolling = false;
+    }
+
+    registerScrollableElement(elementToScroll) {
+        //console.log("Register scroll element: " + elementToScroll);
+        this.scrollDownHandler = (e) => {
+          this.onScrollDown(e, elementToScroll);
+        };
+        if (elementToScroll === null) {
+            return;
+        }
+        elementToScroll.addEventListener("pointerdown", this.scrollDownHandler, true);
+    }
+    
+    unregisterScrollableElement(elementToScroll) {
+        //console.log("Unregister scroll element: " + elementToScroll);
+        if (elementToScroll === null) {
+            return;
+        }
+        elementToScroll.removeEventListener("pointerdown", this.scrollDownHandler);
+    }
+
+    onScrollDown = (e, elementToScroll) => {
+        //console.log("DOWN");
+        if (e.pointerType == "touch") {
+            console.log("touch detected, exit gesture scroll");
+            return;
+        }
+        this.scrollDownPos = [e.clientX, e.clientY];
+        this.scrollLeftDownPos = elementToScroll.scrollLeft;
+        this.scrollTopDownPos = elementToScroll.scrollTop;
+
+        this.scrollTarget = elementToScroll;
+        document.addEventListener("pointermove", this.globalScrollMove);
+        document.addEventListener("pointerup", this.globalScrollUp);
+        document.addEventListener("cancel", this.globalScrollCancel);
+    }
+
+    globalScrollMove = (e) => {
+        //console.log("MOVE");
+        let newMovePos = [e.clientX, e.clientY];
+        let diffX = newMovePos[0] - this.scrollDownPos[0];
+        let diffY = newMovePos[1] - this.scrollDownPos[1];
+
+        let mag = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+        if (mag > this.scrollDistanceThreshold) {
+            this.cancelUpDueToScrolling = true;
+            let newScrollLeft = this.scrollLeftDownPos - diffX;
+            let newScrollTop = this.scrollTopDownPos - diffY;
+            this.scrollTarget.scroll(newScrollLeft, newScrollTop);
+        }
+    }
+
+    globalScrollUp = (e) => {
+        //console.log("UP");
+        this.scrollTarget = null;
+        document.removeEventListener("pointermove", this.globalScrollMove);
+        document.removeEventListener("pointerup", this.globalScrollUp);
+    }
+
+    globalScrollCancel = (e) => {
+        //console.log("CANCEL");
+        this.scrollTarget = null;
+        document.removeEventListener("pointermove", this.globalScrollMove);
+        document.removeEventListener("pointerup", this.globalScrollUp);
+    }
+}
