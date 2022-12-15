a15c02f4320f544004bef5f60872fc5717db2907
Tom Cartwright <tom.cartwright@ultraleap.com>
2022-12-15 11:46:12 +0000

    Add npm workspace and update border example

--- Examples-Web/BorderProgressButtons/src/index.tsx
+++ Examples-Web/BorderProgressButtons/src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import TouchFree from "./TouchFree/TouchFree_Tooling";
+import TouchFree from "TouchFree/src/TouchFree";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 
-TouchFree.Connection.ConnectionManager.init();
+TouchFree.Init();
 
 root.render(
     <React.StrictMode>
