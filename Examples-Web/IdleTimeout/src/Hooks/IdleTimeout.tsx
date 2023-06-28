fa57f02ec542e208f950300ed8bc0ced921dca48
Tom Cartwright <tom.cartwright@ultraleap.com>
2023-06-28 09:55:17 +0100

    Update IdleTimeout

--- Examples-Web/IdleTimeout/src/Hooks/IdleTimeout.tsx
+++ Examples-Web/IdleTimeout/src/Hooks/IdleTimeout.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 
-import { ConnectionManager } from 'TouchFree/src/Connection/ConnectionManager';
+import TouchFree, { EventHandle } from 'TouchFree/src/TouchFree';
 
 export type AppState = 'Idle' | 'Active';
 
 // Custom hook which will return app state based on hand presence.
 // If no hands have been observed for specified timeout in milliseconds
 // the state will be idle.
-// ConnectionManager.init() must be called before this hook.
+// TouchFree.Init() must be called before this hook.
 const useIdleTimeout = (timeoutMs: number): AppState => {
     const [appState, setAppState] = React.useState<AppState>('Idle');
 
@@ -21,12 +21,13 @@ const useIdleTimeout = (timeoutMs: number): AppState => {
         const handLostCallback = () => {
             timeoutId = setTimeout(timeoutFunction, timeoutMs);
         };
-        ConnectionManager.instance.addEventListener('HandFound', handFoundCallback);
-        ConnectionManager.instance.addEventListener('HandsLost', handLostCallback);
+        const handlers: EventHandle[] = [
+            TouchFree.RegisterEventCallback('HandFound', handFoundCallback),
+            TouchFree.RegisterEventCallback('HandsLost', handLostCallback),
+        ];
 
         return () => {
-            ConnectionManager.instance.removeEventListener('HandFound', handFoundCallback);
-            ConnectionManager.instance.removeEventListener('HandLost', handLostCallback);
+            handlers.forEach((handler) => handler.UnregisterEventCallback());
         };
     }, [timeoutMs]);
 
