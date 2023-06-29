88b9f30b227b16cb40a053ed86b0c6fc4a126b3e
Tom Cartwright <tom.cartwright@ultraleap.com>
2023-06-29 15:36:09 +0100

    Refactor ProgressButtons to use CSS gradients

---
+++ Examples-Web/AnimatedProgressButtons/CircularProgress/CircularProgress.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    TouchFree.Init({ initialiseCursor: false });
+    TouchFree.RegisterEventCallback("TransmitInputAction", AnimateCircularProgress);
+};
+
+// Animates a clip path for active overlay elements, identified by the "hovered" id
+function AnimateCircularProgress(inputAction) {
+    const hoveredElement = document.querySelector(".hovered");
+    if (hoveredElement) {
+        const relativeCursorPos = GetRelativeCursorPos(hoveredElement, inputAction.CursorPosition);
+        const progressPercent = EasedProgress(inputAction.ProgressToClick) * 100;
+
+        const root = document.querySelector(":root");
+        root.style.setProperty("--progress", `${progressPercent}%`);
+        root.style.setProperty("--point", `${relativeCursorPos[0]}% ${relativeCursorPos[1]}%`);
+    }
+}
+
+function GetRelativeCursorPos(element, cursorPos) {
+    const bounds = element.getBoundingClientRect();
+    const x = (cursorPos[0] - bounds.left) / bounds.width;
+    const y = (cursorPos[1] - bounds.top) / bounds.height;
+    return [x * 100, y * 100];
+}
+
+function EasedProgress(x) {
+    return 0.1 + CubicEaseIn(x * 0.9);
+}
+
+function CubicEaseIn(x) {
+    return x * x * x;
+}
---
+++ Examples-Web/AnimatedProgressButtons/Hover.js
@@ -0,0 +1,29 @@
+function onEnter(element) {
+    element.classList.add("hovered");
+}
+
+function onLeave(element) {
+    element.classList.remove("hovered");
+    element.classList.remove("active");
+}
+
+function onDown(element) {
+    document.body.style.backgroundColor = element.style.backgroundColor;
+
+    element.classList.remove("hovered");
+    element.classList.add("active");
+}
+
+function onUp(element) {
+    element.classList.remove("active");
+    document.querySelector(":root").style.setProperty("--progress", "0%");
+}
+
+const buttons = document.getElementsByClassName("button");
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener("pointerdown", () => onDown(button));
+    button.addEventListener("pointerup", () => onUp(button));
+    button.addEventListener("pointerenter", () => onEnter(button));
+    button.addEventListener("pointerleave", () => onLeave(button));
+}
